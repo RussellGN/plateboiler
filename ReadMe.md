add7bd5b38138de7e20179d9000522d5f93f3b33
Russellg <dudurussell@gmail.com>
2024-12-27 06:45:38 +0200

    doc: update readme and code documentation comments

--- ReadMe.md
+++ ReadMe.md
@@ -1,15 +1,15 @@
 # Description
 
 This CLI program helps setup various types of dev projects, think
-npm projects and the likes.
+npm web projects projects and the likes.
 
 ## Features
 
-For the time being. It will only setup npm and python projects and allow configuration of common workflows.
+For the time being. It will only setup web-app projects with npm and vite, as well as python django projects.
 
 ## Workflow
 
-walks you through prompts asking for the type of project u want setup and any dependancies along with it,
+walks you through prompts asking for the type of project you want set up and any dependancies along with it,
 similar to more specific framework CLIs
 
 _1 Light dependancy (colored "2.0.0")_
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@
 //! npm projects and the likes.
 //!
 //! ## Features
-//! For the time being. It will only setup npm and python projects and allow configuration of common workflows.
+//! For the time being. It will only setup web-app projects using npm and vite, as well as python django projects.
 //!
 //! ## Workflow
-//! walks you through prompts asking for the type of project u want setup and any dependancies along with it,
+//! walks you through prompts asking for the type of project you want set up and any dependancies along with it,
 //! similar to more specific framework CLIs
 
 use std::process;
@@ -14,7 +14,7 @@ use std::process;
 use plateboiler::{clear_terminal, red_log, yellow_log};
 use plateboiler::{get_program_args, run_program};
 
-const ERROR_EXIT_CODE: i32 = 0; // Not an error exit code, I know. using it so that terminal doesnt print extra text on-exit
+const ERROR_EXIT_CODE: i32 = 0; // Not an error exit code, I know. Using it so that terminal doesnt print extra text on-exit
 
 fn main() {
     clear_terminal();
